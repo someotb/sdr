c4ce71dab2f241a22b3c19482897df386c000157
someotb <llyryk934@gmail.com>
2025-11-18 18:23:26 +0700

    Update main.cpp: fix write_stream func; update graph.py, show.py; catch my own signal

--- pluto/dev/main.cpp
+++ pluto/dev/main.cpp
@@ -24,6 +24,52 @@ struct SDRConfig {
     int16_t *rx_buffer;
 };
 
+struct SDRConfig init(){
+    struct SDRConfig config = {};
+    SoapySDRKwargs args = {};
+    SoapySDRKwargs_set(&args, "driver", "plutosdr");
+    if (1) {
+        SoapySDRKwargs_set(&args, "uri", "usb:");
+    } else {
+        SoapySDRKwargs_set(&args, "uri", "ip:192.168.2.1");
+    }
+    SoapySDRKwargs_set(&args, "direct", "1");
+    SoapySDRKwargs_set(&args, "timestamp_every", "1920");
+    SoapySDRKwargs_set(&args, "loopback", "0");
+    config.sdr = SoapySDRDevice_make(&args);
+    SoapySDRKwargs_clear(&args);
+
+    config.sample_rate = 1e6;
+    config.carrier_freq = 600e6;
+
+    SoapySDRDevice_setSampleRate(config.sdr, SOAPY_SDR_RX, 0, config.sample_rate);
+    SoapySDRDevice_setFrequency(config.sdr, SOAPY_SDR_RX, 0, config.carrier_freq, NULL);
+    SoapySDRDevice_setSampleRate(config.sdr, SOAPY_SDR_TX, 0, config.sample_rate);
+    SoapySDRDevice_setFrequency(config.sdr, SOAPY_SDR_TX, 0, config.carrier_freq, NULL);
+
+    size_t channels = 0;
+    SoapySDRDevice_setGain(config.sdr, SOAPY_SDR_RX, channels, 40.0);
+    SoapySDRDevice_setGain(config.sdr, SOAPY_SDR_TX, channels, -7.0);
+
+    size_t rx_channels[] = {0};
+    size_t tx_channels[] = {0};
+    size_t channel_count = 1;
+
+    config.rxStream = SoapySDRDevice_setupStream(config.sdr, SOAPY_SDR_RX, SOAPY_SDR_CS16, rx_channels, channel_count, NULL);
+    config.txStream = SoapySDRDevice_setupStream(config.sdr, SOAPY_SDR_TX, SOAPY_SDR_CS16, tx_channels, channel_count, NULL);
+
+    SoapySDRDevice_activateStream(config.sdr, config.rxStream, 0, 0, 0);
+    SoapySDRDevice_activateStream(config.sdr, config.txStream, 0, 0, 0);
+
+    config.rx_mtu = SoapySDRDevice_getStreamMTU(config.sdr, config.rxStream);
+    config.tx_mtu = SoapySDRDevice_getStreamMTU(config.sdr, config.txStream);
+
+    config.tx_buff = (int16_t*)calloc(2 * config.tx_mtu, sizeof(int16_t));
+    config.rx_buffer = (int16_t*)calloc(2 * config.rx_mtu, sizeof(int16_t));
+
+    return config;
+}
+
 // QPSK символы
 const complex<double> i0 (0, 0);
 const complex<double> i1 (1, 1);
@@ -41,7 +87,7 @@ static map<string, complex<double>> qpsk_map = {
 template<typename T>
 void Show_Array(const char* title, T *array, int len){
     printf("%s: ", title);
-    for (size_t i = 0; i < len; i++){
+    for (size_t i = 0; i < (size_t)len; i++){
         cout << array[i] << " ";
     }
     printf("\n");
@@ -49,21 +95,21 @@ void Show_Array(const char* title, T *array, int len){
 
 void Mapper(int16_t *bits, int len_b, complex<double> *symbols){
     string pair_bits;
-    for (size_t i = 0; i < len_b; i += 2){
+    for (size_t i = 0; i < (size_t)len_b; i += 2){
         pair_bits = to_string(bits[i]) + to_string(bits[i+1]);
         symbols[i/2] = qpsk_map[pair_bits];
     }
 }
 
 void UpSampler(complex<double> *symbols, int len_s, complex<double> *symbols_ups, int L){
-    for (size_t i = 0; i < len_s*L; i++) symbols_ups[i] = i0;
-    for (size_t i = 0; i < len_s; i++) symbols_ups[i*L] = symbols[i];
+    for (size_t i = 0; i < (size_t)len_s*(size_t)L; i++) symbols_ups[i] = i0;
+    for (size_t i = 0; i < (size_t)len_s; i++) symbols_ups[i*L] = symbols[i];
 }
 
 void filter(complex<double> *symbols_ups, int len_symbols_ups, complex<double> *impulse, int L) {
     vector<complex<double>> sum(len_symbols_ups, 0);
-    for (size_t i = 0; i < len_symbols_ups; i++) {
-        for (size_t j = 0; j < L && (int)(i-j) >= 0; j++) {
+    for (size_t i = 0; i < (size_t)len_symbols_ups; i++) {
+        for (size_t j = 0; j < (size_t)L && (int)(i-j) >= 0; j++) {
             sum[i] += impulse[j] * symbols_ups[i-j];
         }
     }
@@ -71,40 +117,7 @@ void filter(complex<double> *symbols_ups, int len_symbols_ups, complex<double> *
 }
 
 int main(){
-    SDRConfig config = {};
-
-    SoapySDRKwargs args = {};
-    SoapySDRKwargs_set(&args, "driver", "plutosdr");
-    SoapySDRKwargs_set(&args, "uri", "usb:");
-    SoapySDRKwargs_set(&args, "direct", "1");
-    SoapySDRKwargs_set(&args, "timestamp_every", "1920");
-    SoapySDRKwargs_set(&args, "loopback", "0");
-
-    config.sdr = SoapySDRDevice_make(&args);
-    SoapySDRKwargs_clear(&args);
-
-    config.sample_rate = 1e6;
-    config.carrier_freq = 600e6;
-
-    SoapySDRDevice_setSampleRate(config.sdr, SOAPY_SDR_RX, 0, config.sample_rate);
-    SoapySDRDevice_setFrequency(config.sdr, SOAPY_SDR_RX, 0, config.carrier_freq, NULL);
-    SoapySDRDevice_setSampleRate(config.sdr, SOAPY_SDR_TX, 0, config.sample_rate);
-    SoapySDRDevice_setFrequency(config.sdr, SOAPY_SDR_TX, 0, config.carrier_freq, NULL);
-
-    SoapySDRDevice_setGain(config.sdr, SOAPY_SDR_RX, 0, 40.0);
-    SoapySDRDevice_setGain(config.sdr, SOAPY_SDR_TX, 0, -7.0);
-
-    config.rxStream = SoapySDRDevice_setupStream(config.sdr, SOAPY_SDR_RX, SOAPY_SDR_CS16, NULL, 1, NULL);
-    config.txStream = SoapySDRDevice_setupStream(config.sdr, SOAPY_SDR_TX, SOAPY_SDR_CS16, NULL, 1, NULL);
-
-    SoapySDRDevice_activateStream(config.sdr, config.rxStream, 0, 0, 0);
-    SoapySDRDevice_activateStream(config.sdr, config.txStream, 0, 0, 0);
-
-    config.rx_mtu = SoapySDRDevice_getStreamMTU(config.sdr, config.rxStream);
-    config.tx_mtu = SoapySDRDevice_getStreamMTU(config.sdr, config.txStream);
-
-    config.tx_buff = (int16_t*)malloc(2 * config.tx_mtu * sizeof(int16_t));
-    config.rx_buffer = (int16_t*)malloc(2 * config.rx_mtu * sizeof(int16_t));
+    struct SDRConfig config = init();
 
     FILE *tx = fopen("tx.pcm", "wb");
     FILE *rx = fopen("rx.pcm", "wb");
@@ -125,25 +138,25 @@ int main(){
     Mapper(bits, n, symbols);
     UpSampler(symbols, len_symbols, symbols_ups, L);
     filter(symbols_ups, len_symbols_ups, impulse, L);
-    Show_Array("Символы", symbols_ups, len_symbols_ups);
+    // Show_Array("Символы", symbols_ups, len_symbols_ups);
 
-    int16_t *tx_samples = (int16_t*)malloc(2 * len_symbols_ups * sizeof(int16_t));
-    for (size_t i = 0; i < len_symbols_ups; i++) {
-        tx_samples[2*i]     = (int16_t)(real(symbols_ups[i]));
-        tx_samples[2*i + 1] = (int16_t)(imag(symbols_ups[i]));
+    int16_t *tx_samples = (int16_t*)malloc(2 * config.tx_mtu * sizeof(int16_t));
+    for (size_t i = 0; i < (size_t)config.tx_mtu; i++) {
+        tx_samples[2*i] = (int16_t)((real(symbols_ups[i])) * 1000) << 4;
+        tx_samples[2*i + 1] = (int16_t)((imag(symbols_ups[i])) * 1000) << 4;
     }
+    
+    // memcpy(config.tx_buff, tx_samples, 2 * len_symbols_ups * sizeof(int16_t));
     fwrite(tx_samples, sizeof(int16_t), 2 * len_symbols_ups, tx);
 
     const long long timeoutUs = 10000000;
     long long last_time = 0;
-    size_t total_complex_samples = len_symbols_ups;
-    size_t offset = 0;
 
-    while (offset < total_complex_samples){
-        size_t send = min(config.tx_mtu, total_complex_samples - offset);
-        memcpy(config.tx_buff, tx_samples + 2 * offset, 2 * send * sizeof(int16_t));
+    size_t count_of_buffs = 10;
 
+    for (size_t i = 0; i < count_of_buffs; ++i){
         void *rx_buffs[] = {config.rx_buffer};
+        const void *tx_buffs[] = {tx_samples};
         int flags = 0;
         long long timeNs = 0;
 
@@ -151,21 +164,18 @@ int main(){
         if (sr <= 0){
             fprintf(stderr, "Initial RX failed\n");
             break;
-        }
+        }   
 
-        printf("- Samples: %d, Flags: %d, Time: %lld, Diff: %lld\n", sr, flags, timeNs, timeNs - last_time);
+        printf("-Send: %ld, Samples: %d, Flags: %d, Time: %lld, Diff: %lld\n", i, sr, flags, timeNs, timeNs - last_time);
         last_time = timeNs;
 
         long long tx_time = timeNs + (4 * 1000 * 1000); // 4ms в будущее
         flags = SOAPY_SDR_HAS_TIME;
 
-        int st = SoapySDRDevice_writeStream(config.sdr, config.txStream, (const void *const*)&config.tx_buff, send, &flags, tx_time, timeoutUs);
-        if (st != (int)send) fprintf(stderr, "TX short write: expected %zu, got %d\n", send, st);
-
-        offset += send;
+        int st = SoapySDRDevice_writeStream(config.sdr, config.txStream, tx_buffs, config.tx_mtu, &flags, tx_time, timeoutUs);
+        if (st != (int)config.tx_mtu) fprintf(stderr, "TX short write: expected %zu, got %d\n", config.tx_mtu, st);
+        fwrite(rx_buffs[0], sizeof(int16_t), 2 * config.rx_mtu, rx);
     }
-
-    fwrite(config.rx_buffer, sizeof(int16_t), 2 * config.rx_mtu, rx);
 
     SoapySDRDevice_deactivateStream(config.sdr, config.rxStream, 0, 0);
     SoapySDRDevice_deactivateStream(config.sdr, config.txStream, 0, 0);
--- pluto/dev/show.py
+++ pluto/dev/show.py
@@ -1,33 +1,46 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-rx = np.fromfile(f"/home/someotb/code/sdr/pluto/dev/filter2.pcm", dtype=np.int16)
+print("""Choose 0 - Tx
+Choose 1 - Rx""")
+
+answer = int(input())
+
+if answer == 1:
+    nx = np.fromfile(f"/home/someotb/Files/Code/sdr/pluto/dev/build/rx.pcm", dtype=np.int16)
+else:
+    nx = np.fromfile(f"/home/someotb/Files/Code/sdr/pluto/dev/build/tx.pcm", dtype=np.int16)
+
+rx = np.fromfile(f"/home/someotb/Files/Code/sdr/pluto/dev/build/rx.pcm", dtype=np.int16)
+tx = np.fromfile(f"/home/someotb/Files/Code/sdr/pluto/dev/build/tx.pcm", dtype=np.int16)
+print(f"RX: {rx}\nTX: {tx}")
 
 samples = []
 
-for x in range(0, len(rx), 2):
-    samples.append(rx[x] + 1j * rx[x+1])
+for x in range(0, len(nx), 2):
+    samples.append(nx[x] + 1j * nx[x+1])
 
 ampl = np.abs(samples)
 phase = np.angle(samples)
 time = np.arange(len(samples))
 
+# plt.subplot(3,1,1)
+# plt.plot(time, ampl)
+# plt.grid(True)
+# plt.tight_layout()
+# plt.title("Amplitude")
 
-# plot
-plt.subplot(3,1,1)
-plt.legend
-plt.plot(time, ampl)
+# plt.subplot(3,1,2)
+# plt.plot(time, phase)
+# plt.grid(True)
+# plt.tight_layout()
+# plt.title("Phase")
+
+
+# plt.subplot(3,1,3)
+plt.plot(time, nx[0::2])
+# plt.plot(time, nx[1::2])
 plt.grid(True)
-
-plt.subplot(3,1,2)
-plt.legend
-plt.plot(time, phase)
-plt.grid(True)
-
-
-plt.subplot(3,1,3)
-plt.legend
-plt.plot(time, rx[0::2])
-plt.plot(time, rx[1::2])
-plt.grid(True)
+plt.tight_layout()
+plt.title("Real and Imag parts")
 plt.show()
