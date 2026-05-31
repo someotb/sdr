cbdea72d2f586fbf4497bdb42ff90115b75166fe
someotb <llyryk934@gmail.com>
2026-05-31 09:54:32 +0700

    refactor: rm old modules

--- pluto/dev/include/dsp.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "common.hpp"
-
-#include <stop_token>
-
-void run_dsp(std::stop_token stoken, sharedData &sh_data);
--- pluto/dev/include/gui.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "common.hpp"
-
-void run_gui(sharedData &sh_data);
